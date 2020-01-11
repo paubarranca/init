968f37c0edbaff51f0031396900d11d196700c4f
paubarranca <paubarranca91@gmail.com>
2020-01-11 17:18:12 +0000

    Reestructured init

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,53 @@
 #!/bin/bash -e
 
+security_updates(){
+    SCRIPTS_PATH=/usr/local/scripts/
+
+    if [ ! -d "$SCRIPTS_PATH" ]; then
+        mkdir -p $SCRIPTS_PATH
+    fi
+
+    cat << EOF > $SCRIPTS_PATH/security-updates
+#!/bin/bash
+apt list --upgradable | grep -e "-security"; apt list --upgradable | grep -e "-security" | awk -F "/" '{print \$1}' | xargs apt install
+EOF 
+    chown root: $SCRIPTS_PATH/security-updates
+    chmod 700 $SCRIPTS_PATH/security-updates
+    echo '0 2 * * *  root  $SCRIPTS_PATH/security-updates' > /etc/crond.d/security-updates
+}
+
 cp_config(){
-    # logrotate
-    cat /root/init/docker-logrotate > /etc/logrotate.d/docker
+    CONFIG_PATH=/root/init/config/
+
+    # docker logrotate
+    cat $CONFIG_PATH/docker-logrotate > /etc/logrotate.d/docker
+
+    # init logrotate
+    cat $CONFIG_PATH/init-logrotate > /etc/logrotate.d/init
+
+    # monit setup
+    cat $CONFIG_PATH/service-monit > /etc/monit/monitrc
+    /etc/init.d/monit restart
 
     # sysctl tune
-    cat /root/init/sysctl-kernel > /etc/sysctl.conf
+    cat $CONFIG_PATH/sysctl-kernel > /etc/sysctl.conf
 }
 
 docker_install(){
-    LAST_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
-    #COMPOSE_VERSION=1.25.0
+    LATEST_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
 
     echo -e "\nAdding prerequiste packages.... \n\n"
     sudo apt update > /dev/null
-    sudo apt install apt-transport-https net-tools ca-certificates cron fail2ban dbus jq curl gnupg2 -y > /dev/null
+    sudo apt install net-tools ca-certificates cron fail2ban dbus jq curl gnupg2 monit -y > /dev/null
 
     # Install docker
     curl -sSL https://get.docker.com/ | /bin/sh
 
     # Install docker-compose
     echo -e "\nInstalling docker-compose.... \n\n"
-    sh -c "curl -L https://github.com/docker/compose/releases/download/${LAST_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+    sh -c "curl -L https://github.com/docker/compose/releases/download/${LATEST_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
     chmod +x /usr/local/bin/docker-compose
-    sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${LAST_COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+    sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${LATEST_COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
 }
 
 test_compose(){
@@ -46,12 +70,11 @@ fi
 
 cd /root/init
 
-cp_config
-
-docker_install
-
-test_compose
+cp_config; security_updates; docker_install; test_compose
 
 # Cleanup
-#rm -v !("init.sh"|"init.log"|"docker-compose.yml") 
+
+/root/init/cleanup.sh
+
+#find . ! -name 'docker-compose.yml' ! -name 'init.sh' -type f -exec rm -rf {} +
 apt-get -qy --purge autoremove || true
