0d63c591933c7191a2175cf34f18281c1212471a
paubarranca <paubarranca91@gmail.com>
2020-01-11 18:02:07 +0000

    changed cron use

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,7 +14,7 @@ EOF
  
     chown root: $SCRIPTS_PATH/security-updates
     chmod 700 $SCRIPTS_PATH/security-updates
-    cat '0 2 * * *  root  /usr/local/scripts/security-updates > /dev/null' > /etc/crond.d/security-updates
+    echo '0 2 * * *  root  /usr/local/scripts/security-updates > /dev/null' > /etc/crond.d/security-updates
 }
 
 cp_config(){
